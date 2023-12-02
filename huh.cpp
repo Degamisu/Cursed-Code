72237fb0d82a314033e43ac448edd0e3582eaa77
Degamisu <degamisu@gmail.com>
2023-12-01 19:10:17 -0500

    main2

--- huh.cpp
+++ huh.cpp
@@ -4,4 +4,10 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+
+    pthread_key_create(&key, NULL);
+    pthread_setspecific(key, value);
+    get_specific(key); 
+    pthread_setspecific(key, NULL);
+    printf("key: %p, value: %p\n", key, value);
 }
